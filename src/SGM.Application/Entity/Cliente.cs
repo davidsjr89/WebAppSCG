3378399107ff36c3d2c01c79d766dcc6e7f67b23
David Silva Jr <david_sjr@hotmail.com>
2018-06-24 13:47:27 -0300

    Cria classe ClienteContext e adiciona Migrations

---
+++ src/SGM.Application/Entity/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGM.Application.Entity
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public Cliente()
+        {
+
+        }
+
+    }
+}
